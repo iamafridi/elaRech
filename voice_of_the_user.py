6210bd32b37cd0f7f876b57ef54d9eb997f51a62
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-05-29 14:00:54 +0600

    Phase 2.2 Completed and ran final test

--- voice_of_the_user.py
+++ voice_of_the_user.py
@@ -7,6 +7,7 @@ load_dotenv()
 import logging
 import speech_recognition as sr
 from pydub import AudioSegment
+
 from io import BytesIO
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
